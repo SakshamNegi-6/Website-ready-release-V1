b5914aa102133c8bf5e9ee7bd72f38ac7c03d141
SakshamNegi-6 <220175189+SakshamNegi-6@users.noreply.github.com>
2025-10-02 18:22:08 +0530

    Delete functions directory

--- functions/check-password.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function(event) {
-  const REAL_PASSWORD = process.env.ADMIN_PASSWORD;
-  const { attempt } = JSON.parse(event.body);
-  if (attempt === REAL_PASSWORD) {
-    return { statusCode: 200, body: JSON.stringify({ access: 'granted' }) };
-  } else {
-    return { statusCode: 401, body: JSON.stringify({ access: 'denied' }) };
-  }
-};
